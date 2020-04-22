43d11382253e0d05484922a394bbe2ead97e06fc
bluebill1049 <bluebill1049@hotmail.com>
2020-04-22 12:01:16 +1000

    update package json

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react-hooks'],
+  rules: {
+    curly: 'error',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'no-console': 'error',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
---
+++ src/yup.ts
@@ -0,0 +1,71 @@
+import { appendErrors, transformToNestObject } from 'react-hook-form';
+
+type YupValidationError = {
+  inner: { path: string; message: string; type: string }[];
+  path: string;
+  message: string;
+  type: string;
+};
+
+type FieldValues = Record<string, any>;
+
+export const parseErrorSchema = (
+  error: YupValidationError,
+  validateAllFieldCriteria: boolean,
+) =>
+  Array.isArray(error.inner)
+    ? error.inner.reduce(
+        (previous: FieldValues, { path, message, type }: FieldValues) => ({
+          ...previous,
+          ...(path
+            ? previous[path] && validateAllFieldCriteria
+              ? {
+                  [path]: appendErrors(
+                    path,
+                    validateAllFieldCriteria,
+                    previous,
+                    type,
+                    message,
+                  ),
+                }
+              : {
+                  [path]: previous[path] || {
+                    message,
+                    type,
+                    ...(validateAllFieldCriteria
+                      ? {
+                          types: { [type]: message || true },
+                        }
+                      : {}),
+                  },
+                }
+            : {}),
+        }),
+        {},
+      )
+    : {
+        [error.path]: { message: error.message, type: error.type },
+      };
+
+export const yup = (
+  validationSchema: any,
+  config: any = {
+    abortEarly: false,
+  },
+) => async (data: any, _: any = {}, validateAllFieldCriteria = false) => {
+  try {
+    return {
+      values: await validationSchema.validate(data, {
+        ...config,
+      }),
+      errors: {},
+    };
+  } catch (e) {
+    return {
+      values: {},
+      errors: transformToNestObject(
+        parseErrorSchema(e, validateAllFieldCriteria),
+      ),
+    };
+  }
+};
