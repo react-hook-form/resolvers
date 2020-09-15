409b4947925e750719f74e1070bc5988223e9563
Joris <reixjoris@gmail.com>
2020-09-15 13:56:08 +0200

    feat: add zod resolver

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './yup';
 export * from './superstruct';
 export * from './joi';
 export * from './jsonSchema';
+export * from './zod';
---
+++ src/zod.ts
@@ -0,0 +1,81 @@
+import {
+  appendErrors,
+  FieldValues,
+  Resolver,
+  ResolverError,
+  ResolverSuccess,
+  transformToNestObject,
+} from 'react-hook-form';
+import { Schema, ZodError } from 'zod';
+import { ParseParams } from 'zod/lib/src/parser';
+import convertArrayToPathName from './utils/convertArrayToPathName';
+
+const parseErrorSchema = (
+  zodError: ZodError,
+  validateAllFieldCriteria: boolean,
+) => {
+  if (!Array.isArray(zodError.errors) || zodError.isEmpty) {
+    return {};
+  }
+
+  return zodError.errors.reduce<Record<string, any>>(
+    (previous, { path, message, code: type }) => {
+      const currentPath = convertArrayToPathName(path);
+
+      previous[currentPath] = {
+        message,
+        type,
+      };
+
+      return {
+        ...previous,
+        ...(path
+          ? previous[currentPath] && validateAllFieldCriteria
+            ? {
+                [currentPath]: appendErrors(
+                  currentPath,
+                  validateAllFieldCriteria,
+                  previous,
+                  type || '',
+                  message,
+                ),
+              }
+            : {
+                [currentPath]: previous[currentPath] || {
+                  message,
+                  type,
+                  ...(validateAllFieldCriteria
+                    ? {
+                        types: { [type || '']: message || true },
+                      }
+                    : {}),
+                },
+              }
+          : {}),
+      };
+    },
+    {},
+  );
+};
+
+export const zodResolver = <TFieldValues extends FieldValues>(
+  schema: Schema<any>,
+  options?: ParseParams,
+): Resolver<TFieldValues> => async (
+  values,
+  _,
+  validateAllFieldCriteria = false,
+) => {
+  const result = schema.safeParse(values, options);
+
+  if (result.success) {
+    return { values: result.data, errors: {} } as ResolverSuccess<TFieldValues>;
+  }
+
+  return {
+    values: {},
+    errors: transformToNestObject(
+      parseErrorSchema(result.error, validateAllFieldCriteria),
+    ),
+  } as ResolverError<TFieldValues>;
+};
