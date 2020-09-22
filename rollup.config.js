ec71b13a5101140b7d235b95462a42d5500f629e
Joris <reixjoris@gmail.com>
2020-09-22 11:05:57 +0200

    refactor: remove useless code

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import { createRollupConfig } from './rollup/createRollupConfig';
-import pkg from './package.json';
 
 const name = 'index';
 const umdName = 'ReactHookFormResolvers';
@@ -8,42 +7,32 @@ const options = [
     name,
     umdName,
     format: 'cjs',
-    env: 'development',
-    input: pkg.source,
   },
   {
     name,
     umdName,
     format: 'cjs',
-    env: 'production',
-    input: pkg.source,
   },
   {
     name,
     umdName,
     format: 'esm',
-    input: pkg.source,
   },
   {
     name,
     umdName,
     format: 'umd',
-    env: 'development',
-    input: pkg.source,
   },
   {
     name,
     umdName,
     format: 'umd',
-    env: 'production',
-    input: pkg.source,
   },
   {
     name,
     umdName,
     format: 'esm',
     formatName: 'ie11',
-    input: pkg.source,
     tsconfig: 'tsconfig.ie11.json',
   },
 ];
