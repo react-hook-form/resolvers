627cc39f78266527b47ed1f40f1590ff3ea0b9cd
jorisre <reixjoris@gmail.com>
2021-01-05 23:18:29 +0100

    test: add components test to ensure TypeScript is working as expected
    Related to #97

---
+++ vest/src/__tests__/Form.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { useForm } from 'react-hook-form';
+import vest from 'vest';
+import { vestResolver } from '..';
+
+interface FormData {
+  username: string;
+  password: string;
+}
+
+const validationSuite = vest.create('form', (data: FormData) => {
+  vest.test('username', 'Username is required', () => {
+    vest.enforce(data.username).isNotEmpty();
+  });
+
+  vest.test('password', 'Password must contain a symbol', () => {
+    vest.enforce(data.password).matches(/[^A-Za-z0-9]/);
+  });
+});
+
+interface Props {
+  onSubmit: (data: FormData) => void;
+}
+
+function TestComponent({ onSubmit }: Props) {
+  const { register, errors, handleSubmit } = useForm<FormData>({
+    resolver: vestResolver(validationSuite), // Useful to check TypeScript regressions
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="username" ref={register} />
+      {errors.username && <span role="alert">{errors.username.message}</span>}
+
+      <input name="password" ref={register} />
+      {errors.password && <span role="alert">{errors.password.message}</span>}
+
+      <button type="submit">submit</button>
+    </form>
+  );
+}
+
+test("form's validation with Vest and TypeScript's integration", async () => {
+  const handleSubmit = jest.fn();
+  render(<TestComponent onSubmit={handleSubmit} />);
+
+  expect(screen.queryAllByRole(/alert/i)).toHaveLength(0);
+
+  await act(async () => {
+    user.click(screen.getByText(/submit/i));
+  });
+
+  expect(screen.getByText(/Username is required/i)).toBeInTheDocument();
+  expect(
+    screen.getByText(/Password must contain a symbol/i),
+  ).toBeInTheDocument();
+  expect(handleSubmit).not.toHaveBeenCalled();
+});
