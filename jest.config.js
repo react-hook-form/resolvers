d081ff4c58a559d646cb60c68eff61b050410050
jorisre <reixjoris@gmail.com>
2021-02-02 23:07:59 +0100

    test: jest fixtures config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   restoreMocks: true,
   testMatch: ['**/__tests__/**/*.+(js|jsx|ts|tsx)'],
+  testPathIgnorePatterns: ['/__fixtures__/'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   moduleNameMapper: {
     '^@hookform/resolvers$': '<rootDir>/src',
