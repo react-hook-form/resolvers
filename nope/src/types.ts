7a6d7366d6e976b97c48d22ab0f2aca02f0c58de
Joris <reixjoris@gmail.com>
2021-05-08 12:05:54 +0200

    chore: update deps

--- nope/src/types.ts
+++ nope/src/types.ts
@@ -4,7 +4,7 @@ import type {
   ResolverResult,
   UnpackNestedValue,
 } from 'react-hook-form';
-import type NopeObject from 'nope-validator/lib/cjs/NopeObject';
+import type { NopeObject } from 'nope-validator/lib/cjs/NopeObject';
 
 type ValidateOptions = Parameters<NopeObject['validate']>[2];
 type Context = Parameters<NopeObject['validate']>[1];
