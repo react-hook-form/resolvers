27fb3624b1cddef5091f4440ce8058d3cffc0601
KATT <alexander@n1s.se>
2022-05-13 11:22:46 +0200

    align coding style

--- zod/src/types.ts
+++ zod/src/types.ts
@@ -6,22 +6,20 @@ import {
 } from 'react-hook-form';
 import { z } from 'zod';
 
-interface FactoryOptions {
-  /**
-   * @default async
-   */
-  mode?: 'async' | 'sync';
-  /**
-   * Return the raw input values rather than the parsed values.
-   * @default false
-   */
-  rawValues?: boolean;
-}
-
 export type Resolver = <T extends z.Schema<any, any>>(
   schema: T,
   schemaOptions?: Partial<z.ParseParams>,
-  factoryOptions?: FactoryOptions,
+  factoryOptions?: {
+    /**
+     * @default async
+     */
+    mode?: 'async' | 'sync';
+    /**
+     * Return the raw input values rather than the parsed values.
+     * @default false
+     */
+    rawValues?: boolean;
+  },
 ) => <TFieldValues extends FieldValues, TContext>(
   values: UnpackNestedValue<TFieldValues>,
   context: TContext | undefined,
