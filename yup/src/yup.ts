928afae6ea1c9174e591953c008d9c8c86a43e57
Adrien Denat <adrien.denat@gmail.com>
2022-07-15 04:30:06 -0300

    fix(yup): yup wrong import statement assuming default export (#427)

--- yup/src/yup.ts
+++ yup/src/yup.ts
@@ -1,4 +1,4 @@
-import Yup from 'yup';
+import * as Yup from 'yup';
 import { toNestError, validateFieldsNatively } from '@hookform/resolvers';
 import { appendErrors, FieldError } from 'react-hook-form';
 import { Resolver } from './types';
