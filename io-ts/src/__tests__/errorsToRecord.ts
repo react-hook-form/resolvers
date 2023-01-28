4991c055c20440ad0dc766243e6feff38b871d55
Igor Loskutov <igor.loskutoff@gmail.com>
2023-01-28 15:22:15 +0700

    prettier types in io-ts tests

--- io-ts/src/__tests__/errorsToRecord.ts
+++ io-ts/src/__tests__/errorsToRecord.ts
@@ -2,7 +2,7 @@ import * as t from 'io-ts';
 import { isLeft } from 'fp-ts/Either';
 import errorsToRecord from '../errorsToRecord';
 
-const assertLeft = (result: t.Validation<any>) => {
+const assertLeft = <T>(result: t.Validation<T>) => {
   expect(isLeft(result)).toBe(true);
   if (!isLeft(result)) {throw new Error('panic! error is not of the "left" type, should be unreachable');}
   return result.left;
@@ -21,7 +21,7 @@ describe('errorsToRecord', () => {
       })
     ]));
     const error = assertLeft(schema.decode({}));
-    const record = errorsToRecord(false)(error)
-    expect(record[FIRST_NAME_FIELD_PATH]).toMatchObject({message: 'expected string but got undefined', type: 'string'})
+    const record = errorsToRecord(false)(error);
+    expect(record[FIRST_NAME_FIELD_PATH]).toMatchObject({message: 'expected string but got undefined', type: 'string'});
   });
 });
