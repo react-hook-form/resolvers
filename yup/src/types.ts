f3da2123b081f87eb3dae06f7a0c2d84fb9eb39c
Joris <reixjoris@gmail.com>
2023-03-19 16:11:43 +0100

    feat(yupResolver): add support Yup v1 (#520)
    BREAKING CHANGE: require Yup v1

--- yup/src/types.ts
+++ yup/src/types.ts
@@ -1,19 +1,12 @@
-import {
-  FieldValues,
-  ResolverOptions,
-  ResolverResult,
-} from 'react-hook-form';
+import { FieldValues, ResolverOptions, ResolverResult } from 'react-hook-form';
 import * as Yup from 'yup';
-import type Lazy from 'yup/lib/Lazy';
 
-type Options<T extends Yup.AnyObjectSchema | Lazy<any>> = Parameters<
-  T['validate']
->[1];
+type Options<T extends Yup.ObjectSchema<any>> = Parameters<T['validate']>[1];
 
-export type Resolver = <T extends Yup.AnyObjectSchema | Lazy<any>>(
+export type Resolver = <T extends Yup.ObjectSchema<any>>(
   schema: T,
   schemaOptions?: Options<T>,
-  factoryOptions?: { mode?: 'async' | 'sync', rawValues?: boolean; },
+  factoryOptions?: { mode?: 'async' | 'sync'; rawValues?: boolean },
 ) => <TFieldValues extends FieldValues, TContext>(
   values: TFieldValues,
   context: TContext | undefined,
