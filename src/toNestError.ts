6f7c3ebcee13961a3c6f7a25ba2e44aaeb36634b
Joris <reixjoris@gmail.com>
2023-08-20 11:53:03 +0200

    feat: add support for root errors for field array

--- src/toNestError.ts
+++ src/toNestError.ts
@@ -5,6 +5,7 @@ import {
   Field,
   ResolverOptions,
   FieldValues,
+  InternalFieldName,
 } from 'react-hook-form';
 import { validateFieldsNatively } from './validateFieldsNatively';
 
@@ -17,13 +18,30 @@ export const toNestError = <TFieldValues extends FieldValues>(
   const fieldErrors = {} as FieldErrors<TFieldValues>;
   for (const path in errors) {
     const field = get(options.fields, path) as Field['_f'] | undefined;
+    const error = Object.assign(errors[path] || {}, {
+      ref: field && field.ref,
+    });
 
-    set(
-      fieldErrors,
-      path,
-      Object.assign(errors[path] || {}, { ref: field && field.ref }),
-    );
+    if (isNameInFieldArray(options.names || Object.keys(errors), path)) {
+      const fieldArrayErrors = Object.assign(
+        {},
+        compact(get(fieldErrors, path)),
+      );
+
+      set(fieldArrayErrors, 'root', error);
+      set(fieldErrors, path, fieldArrayErrors);
+    } else {
+      set(fieldErrors, path, error);
+    }
   }
 
   return fieldErrors;
 };
+
+const compact = <TValue>(value: TValue[]) =>
+  Array.isArray(value) ? value.filter(Boolean) : [];
+
+const isNameInFieldArray = (
+  names: InternalFieldName[],
+  name: InternalFieldName,
+) => names.some((n) => n.startsWith(name + '.'));
