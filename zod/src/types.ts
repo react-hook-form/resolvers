8fcd6f74f08185642b3c668f2af8d065f70167a1
Nick Dobner <mintchkin.dobfner@gmail.com>
2023-12-09 21:35:02 +0000

    fix(zod): allow inferring specific `Resolver` type from `zodResolver`

--- zod/src/types.ts
+++ zod/src/types.ts
@@ -1,22 +1,4 @@
-import { FieldValues, ResolverResult, ResolverOptions } from 'react-hook-form';
-import { z } from 'zod';
+import type { zodResolver } from './zod';
 
-export type Resolver = <T extends z.Schema<any, any>>(
-  schema: T,
-  schemaOptions?: Partial<z.ParseParams>,
-  factoryOptions?: {
-    /**
-     * @default async
-     */
-    mode?: 'async' | 'sync';
-    /**
-     * Return the raw input values rather than the parsed values.
-     * @default false
-     */
-    raw?: boolean;
-  },
-) => <TFieldValues extends FieldValues, TContext>(
-  values: TFieldValues,
-  context: TContext | undefined,
-  options: ResolverOptions<TFieldValues>,
-) => Promise<ResolverResult<TFieldValues>>;
+
+export type Resolver = ReturnType<typeof zodResolver>
