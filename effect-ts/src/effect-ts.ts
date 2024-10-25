d703896fbccb84fcd00a36de25fa52815c7b8878
André König <andre.koenig@openformation.io>
2024-10-25 13:49:24 +0200

    chore(effect-ts): updates to the latest version

--- effect-ts/src/effect-ts.ts
+++ effect-ts/src/effect-ts.ts
@@ -1,6 +1,6 @@
-import { formatIssue } from '@effect/schema/ArrayFormatter';
-import { decodeUnknown } from '@effect/schema/ParseResult';
 import { toNestErrors, validateFieldsNatively } from '@hookform/resolvers';
+import { Schema } from 'effect';
+import { ParseResult } from 'effect';
 import * as Effect from 'effect/Effect';
 import type { FieldErrors } from 'react-hook-form';
 import type { Resolver } from './types';
@@ -8,11 +8,13 @@ import type { Resolver } from './types';
 export const effectTsResolver: Resolver =
   (schema, config = { errors: 'all', onExcessProperty: 'ignore' }) =>
   (values, _, options) => {
-    return decodeUnknown(
+    return Schema.decodeUnknown(
       schema,
       config,
     )(values).pipe(
-      Effect.catchAll((parseIssue) => Effect.flip(formatIssue(parseIssue))),
+      Effect.catchTag('ParseError', (parseIssue) =>
+        Effect.flip(ParseResult.ArrayFormatter.formatError(parseIssue)),
+      ),
       Effect.mapError((issues) => {
         const errors = issues.reduce((acc, current) => {
           const key = current.path.join('.');
