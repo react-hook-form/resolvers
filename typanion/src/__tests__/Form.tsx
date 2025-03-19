cfcafe290b6b29677ef6767d7572b109efdb366d
Joris <reixjoris@gmail.com>
2025-03-19 12:15:20 +0100

    feat(typanion): types inference

--- typanion/src/__tests__/Form.tsx
+++ typanion/src/__tests__/Form.tsx
@@ -10,12 +10,6 @@ const schema = t.isObject({
   password: t.cascade(t.isString(), [t.hasMinLength(1)]),
 });
 
-interface FormData {
-  unusedProperty: string;
-  username: string;
-  password: string;
-}
-
 function TestComponent({
   onSubmit,
 }: { onSubmit: (data: t.InferType<typeof schema>) => void }) {
@@ -55,29 +49,3 @@ test("form's validation with Typanion and TypeScript's integration", async () =>
   ).toHaveLength(2);
   expect(handleSubmit).not.toHaveBeenCalled();
 });
-
-export function TestComponentManualType({
-  onSubmit,
-}: {
-  onSubmit: (data: FormData) => void;
-}) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<t.InferType<typeof schema>, undefined, FormData>({
-    resolver: typanionResolver(schema), // Useful to check TypeScript regressions
-  });
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('username')} />
-      {errors.username && <span role="alert">{errors.username.message}</span>}
-
-      <input {...register('password')} />
-      {errors.password && <span role="alert">{errors.password.message}</span>}
-
-      <button type="submit">submit</button>
-    </form>
-  );
-}
